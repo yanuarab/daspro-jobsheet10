78d919bc132db7b7965818fc1cfb77d5fbff56e9
tugas <aldayanuar6@gmail.com>
2024-11-07 23:09:55 +0700

    percobaan 1 modifikasi

--- Bioskop25modif.java
+++ Bioskop25modif.java
@@ -17,6 +17,11 @@ public class Bioskop25modif {
     System.out.println("Panjang baris 3: " + penonton[2].length); 
     System.out.println("Panjang baris 4: " + penonton[3].length);
 
+   System.out.println(penonton.length);
+   for (int i = 0; i < penonton.length; i++) {
+    System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+   }
+
    System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
    System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
    System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
