9911118483b65ea59e1d1ba90a6e293d6a492357
tugas <aldayanuar6@gmail.com>
2024-11-08 10:10:18 +0700

    percobaan 3

---
+++ Numbers25.java
@@ -0,0 +1,9 @@
+public class Numbers25 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+        
+    }
+}
