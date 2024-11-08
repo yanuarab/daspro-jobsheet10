bdffae599e8a8ec6cec192c0c3aa5b70be8cfdb0
tugas <aldayanuar6@gmail.com>
2024-11-08 10:16:45 +0700

    percobaan 3 modifikasi

--- Numbers25modif.java
+++ Numbers25modif.java
@@ -10,6 +10,9 @@ public class Numbers25modif {
          for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
         }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + myNumbers[i].length);
+        }
 
     }
 }
